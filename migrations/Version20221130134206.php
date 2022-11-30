e38609489667bd3e6727c5f87d0f1a76365d6db9
ThomasBaudon <tbaudon@yahoo.fr>
2022-11-30 14:43:43 +0100

    migrations

---
+++ migrations/Version20221130134206.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221130134206 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE company ADD sigle VARCHAR(3) NOT NULL, ADD employes INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE company DROP sigle, DROP employes');
+    }
+}
